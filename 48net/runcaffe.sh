9654d10acebda91dab1113f08f97543acdbc482d
huajianni666 <huajianni666@github.com>
2018-05-15 06:42:13 +0000

    rm

--- 48net/runcaffe.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-export PYTHONPATH=$PYTHONPATH:/workspace/run/huajianni/O-NET/48net
-./../../RefineDet/build/tools/caffe train \
-	 --solver=solver.prototxt \
-  	 --weights=48net.caffemodel \
-         --gpu 0,1  2>&1 | tee jobs/onet_train.log
